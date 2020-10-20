5aadff25f27677efa08e2cb3450ba8191721c1ed
igor-siciliani <igorschoeller@gmail.com>
2020-10-20 12:39:02 -0300

    testes js

---
+++ R/testes.R
@@ -0,0 +1,27 @@
+jsCode = glue("
+  var {asyncheck} = true;
+
+  SetInterval(function({input},{asyncheck}){
+     if({asyncheck} == true):
+        Shiny.addCustomMessageHandler(input , function(){
+        Shiny.onInputChange(input,'_' + Math.random().toString(36).substr(2, 9));
+    }, {timer},{input});
+")
+
+
+jsCode2 = glue("
+     var {asyncheck} = false;
+")
+
+
+async$input = 'var2'
+
+
+async_bar = function(async,ID=uuid::UUIDgenerate()){
+
+  activate = async$input
+  shiny::observeEvent(input$var2,)
+
+
+}
+
