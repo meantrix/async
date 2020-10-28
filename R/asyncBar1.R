9bd980fc993a45a467713085734f46a2208ece2c
igor-siciliani <igorschoeller@gmail.com>
2020-10-28 09:16:07 -0300

    detail asyncBar

--- R/asyncBar1.R
+++ R/asyncBar1.R
@@ -4,22 +4,16 @@
 #' @name asyncBar1
 #' @export
 NULL
-
 asyncBar1 = R6::R6Class(classname = 'asyncBar1',
-
                         private = list(
-
                           async = NULL,
                           input = NULL,
                           id = NULL,
                           fun.id = NULL,
-                          detail = "",
                           interval = NULL,
                           last.val = NULL,
                           max.rep = NULL,
                           n.rep = 0,
-
-
                           progress_new = function(id , message , detail , show=TRUE){
                             return(
                               paste0(
@@ -53,7 +47,6 @@ asyncBar1 = R6::R6Class(classname = 'asyncBar1',
                               )
                             )
                           },
-
                           progress_inc = function(id , width){
                             return(
                               paste0(
@@ -63,7 +56,6 @@ asyncBar1 = R6::R6Class(classname = 'asyncBar1',
                               )
                             )
                           },
-
                           progress_set = function(id , message , detail , width){
                             return(
                               paste0("((id , message , detail , width) => {",
@@ -74,102 +66,72 @@ asyncBar1 = R6::R6Class(classname = 'asyncBar1',
                               )
                             )
                           },
-
-
                           progress_showbar = function(id){
                             return(paste0("((id) => { $('#shiny-notification-' + id).find('.progress.progress-striped.active').show(); })('" , id , "'); "))
                           },
-
-
                           progress_hidebar = function(id) {
                             return(paste0("((id) => { $('#shiny-notification-' + id).find('.progress.progress-striped.active').hide(); })('" , id , "'); "))
                           },
-
-
                           progress_close = function(id){
                             return(paste0("$('#shiny-notification-" , id , "').remove(); "))
                           },
-
                           interrupt_client = function(){
                             fun.id = private$fun.id
                             jsCode = paste0("clearInterval(window.",fun.id,");")
                             shinyjs::runjs(jsCode)
                             shinyjs::runjs(private$progress_close(private$id))
                             #unlink(private$async$status_file)
-
                           },
-
-                          create_progress  = function(msg){
+                          create_progress  = function(){
                             #browser()
-                            if(missing(msg)){ msg = '' }
-
+                            vars = private$async$.__enclos_env__$private$get_status()
+                            detail = as.character(vars[3])
+                            msg = as.character(vars[2])
+                            val = as.numeric(vars[1])
                             fun.id = private$fun.id
                             input = private$input
                             timer = private$interval
-
-                              jsCode = paste0(fun.id," = setInterval(function(){",
+                            jsCode = paste0(fun.id," = setInterval(function(){",
                                             "Shiny.onInputChange('",input,"','_' + Math.random().toString(36).substr(2, 9));",
                                             "},",timer,");")
                             shinyjs::runjs(jsCode)
                             shinyjs::runjs(private$progress_new(id = private$id,
-                                                                detail = private$detail,
+                                                                detail = detail,
                                                                 message = msg))
                             shinyjs::runjs(private$progress_showbar(id = private$id))
-
                           },
-
                           observe_event = function(session,input){
-
                             shiny::observeEvent(input[[private$input]],{
                               #browser()
                               vars = private$async$status()
+                              detail = as.character(vars[3])
                               msg = as.character(vars[2])
                               val = as.numeric(vars[1])
-
                               max.val = private$async$upper
                               min.val = private$async$lower
-
                               val = ifelse(is.numeric(val),val,private$last.val)
                               msg = ifelse(is.character(msg),msg,'')
-
+                              detail = ifelse(is.character(detail),detail,'')
                               if( isTRUE(val == private$last.val) ){
-
                                 private$n.rep = private$n.rep + 1
-
                               } else {
-
                                 private$n.rep = 0
-
                               }
-
                               private$last.val = val
-
                               if( isTRUE(val == 7777) |
                                   isTRUE(val == max.val) |
                                   isTRUE(private$n.rep > private$max.rep) ) {
-
                                 private$interrupt_client()
-
-
                               } else {
-
                                 width = (val - min.val)/(max.val-min.val)
-
                                 shinyjs::runjs(private$progress_set(id = private$id ,
                                                                     message = msg ,
-                                                                    detail = private$detail,
+                                                                    detail = detail,
                                                                     width = width))
                               }
-
                             },domain = session)
-
-
                           }
-
-
-
                         ),
-
                         public = list(
                           #' @description
                           #' Interactive environment
@@ -180,22 +142,17 @@ asyncBar1 = R6::R6Class(classname = 'asyncBar1',
                           #' to wait between checks of the job progress.
                           #' @param max.rep numeric. Maximum number of times that
                           #' a progress value can be repeated.
-                          #' @param detail detail message to display.
                           initialize = function(async,
                                                 id,
                                                 interval=1000,
-                                                max.rep = 50,
-                                                detail=''){
-
+                                                max.rep = 50){
                             checkmate::expect_class(async,'R6')
                             checkmate::expect_character(id,max.len = 1)
                             checkmate::expect_character(detail,max.len = 1)
                             checkmate::expect_numeric(interval,lower = 0,upper = Inf)
                             checkmate::expect_numeric(max.rep,lower = 1,upper = Inf)
-
                             vars.id = do.call(paste0, Map(stringi::stri_rand_strings, n=2, length=c(5, 4, 1),
                                                           pattern = c('[A-Z]', '[0-9]', '[A-Z]')))
-
                             private$input = vars.id[1]
                             private$fun.id = vars.id[2]
                             private$id = id
@@ -203,33 +160,25 @@ asyncBar1 = R6::R6Class(classname = 'asyncBar1',
                             private$interval = interval
                             private$async = async
                             private$detail = detail
-
                             vars.status = as.numeric(async$.__enclos_env__$private$get_status())
                             last.val  = vars.status[1]
-                            msg = ifelse(is.character(vars.status[2]),vars.status[2],'')
                             private$last.val = ifelse(is.numeric(last.val),last.val,async$lower)
-                            private$create_progress(msg = msg)
+                            private$create_progress()
                           },
                           #' @description
                           #' observe event to checks of the async job progress.
                           #' @param session shiny session
                           #' @param input shiny input
                           progress = function(session,input) {
-
                             if(missing(session)){
                               session = shiny::getDefaultReactiveDomain()
                             }
-
                             private$observe_event(session,input)
-
                           },
                           #' @description
                           #' Close all routines of async bar.
                           finalize = function(){
                             private$interrupt_client()
-
                           }
-
                         )
-
 )
